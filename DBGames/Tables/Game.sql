d86695c4b0a18cc0e2e126b04616a8c62d319fcb
jenniferemilie <jenniferhoyez@gmail.com>
2023-08-07 13:06:52 +0200

    Ajoutez des fichiers projet.

---
+++ DBGames/Tables/Game.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Game]
+(
+	[ID_GAME] INT NOT NULL IDENTITY PRIMARY KEY,
+	[Name_Game] VARCHAR (100) NOT NULL UNIQUE,
+	[Editor_Game] VARCHAR (100) ,
+	[Release_Year] INT,
+	[Player_Age] INT,
+	[Last_Part_Of_The_Game] INT NOT NULL,
+	[Player_Number] INT ,
+	[Resum_Game] VARCHAR(8000),
+	[Code_Ean] INT 
+)
